27703c8be76f3c48486b038423449f6c95bf7385
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-06-10 09:45:55 -0300

    FEAT - Busca todas as urls de um usuário.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,13 @@ import { Router } from "express";
 
 import registerRouter from "./registerRouter.js";
 import urlsRouter from "./urlsRouter.js";
+import usersRouter from "./usersRouter.js";
+
 
 const router = Router();
 
 router.use(registerRouter);
 router.use(urlsRouter);
+router.use(usersRouter);
 
 export default router;
