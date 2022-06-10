09cebda04db6c141561d1bcfe4c8a77e20a96160
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-06-10 11:11:16 -0300

    DEPLOY - configurações para deploy

--- src/db.js
+++ src/db.js
@@ -5,8 +5,15 @@ dotenv.config();
 
 const {Pool} = pg;
 
-const db = new Pool({
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool({
+    databaseConfig
 });
 
 export default db;
