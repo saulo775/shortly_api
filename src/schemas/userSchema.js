78171d693047f00aa4a2f8da8b39a38cfd997dc0
Mateus Brasil <Mateus-code-98>
2022-06-21 11:53:14 -0300

    Preparando revisão de código

--- src/schemas/userSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const userSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(5).required()
-})
-
-export default userSchema;
